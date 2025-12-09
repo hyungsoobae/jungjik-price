104b3618402f65e2c309706cbcdc0622a0fb76e1
hs.bae <hs950823@gmail.com>
2025-12-09 18:53:36 +0900

    first commit

---
+++ src/main/java/com/zz95/jungjik/JungjikApplication.java
@@ -0,0 +1,13 @@
+package com.zz95.jungjik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JungjikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JungjikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zz95/jungjik/JungjikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zz95.jungjik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JungjikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

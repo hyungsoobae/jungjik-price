709c60feac7dd2f992b1e987e24b02bf1e202cb1
DESKTOP-PKD7SQL\배형수 <hs950823@gmail.com>
2026-02-02 17:01:43 +0900

    feat: 상품 가격 이력 수집 스케줄러 추가

--- src/main/java/com/zz95/jungjik/JungjikApplication.java
+++ src/main/java/com/zz95/jungjik/JungjikApplication.java
@@ -2,8 +2,10 @@ package com.zz95.jungjik;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class JungjikApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/zz95/jungjik/schedule/PriceCollectScheduler.java
@@ -0,0 +1,28 @@
+package com.zz95.jungjik.schedule;
+
+import com.zz95.jungjik.domain.price.PriceHistoryService;
+import com.zz95.jungjik.domain.product.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class PriceCollectScheduler {
+
+    private final ProductRepository productRepository;
+    private final PriceHistoryService priceHistoryService;
+
+    /**
+     * 매시 30분마다 활성화된 상품 가격 수집
+     */
+    @Scheduled(cron = "0 30 * * * *")
+    public void collectPrices() {
+        log.info("PriceCollectScheduler.collectPrices : START ");
+
+        productRepository.findByIsActiveTrue()
+                .forEach(priceHistoryService::collect);
+    }
+}

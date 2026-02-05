af563b9b0cd266cf9cb1afdf2e8e6c4b04f9d031
hs.bae <hs950823@gmail.com>
2026-02-05 22:45:07 +0900

    feat: GlobalExceptionHandler에 @Valid 예외 핸들러 구현

--- src/main/java/com/zz95/jungjik/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/zz95/jungjik/global/error/GlobalExceptionHandler.java
@@ -3,7 +3,9 @@ package com.zz95.jungjik.global.error;
 import com.zz95.jungjik.global.common.ApiResponse;
 import com.zz95.jungjik.global.error.exception.BusinessException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -33,5 +35,20 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(errorCode.getStatus())
                 .body(ApiResponse.error(errorCode));
+    }
+
+    /**
+     * @Valid 검증 실패 시 발생하는 예외 처리
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<ApiResponse<Void>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error("MethodArgumentNotValidException", e);
+
+        // 에러 메시지 중 첫 번째 항목을 가져옴
+        String errorMessage = e.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ApiResponse.error(ErrorCode.INVALID_INPUT_VALUE, errorMessage));
     }
 }

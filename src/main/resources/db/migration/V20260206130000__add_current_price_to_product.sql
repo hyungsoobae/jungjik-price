f1d6530ea4c4c1d552d6f22a9d6c30a637e014bf
hs.bae <hs950823@gmail.com>
2026-02-16 15:34:20 +0900

    fix: product.current_price 명시적으로 NOT NULL 추가

--- src/main/resources/db/migration/V20260206130000__add_current_price_to_product.sql
+++ src/main/resources/db/migration/V20260206130000__add_current_price_to_product.sql
@@ -1,2 +1,2 @@
-ALTER TABLE product ADD COLUMN current_price INT DEFAULT 0;
+ALTER TABLE product ADD COLUMN current_price INT NOT NULL DEFAULT 0;
 COMMENT ON COLUMN product.current_price IS '현재 최신 가격';
